5aed81c41d45234864b5e7bcfef5a9c39ae43d86
YANJUN-WANG <180739747+wangyanjun1999@users.noreply.github.com>
2025-01-23 22:34:50 +0100

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("你好,世界!");
+    }
+
+}
+
+class PrivateClassMain{
+    public static void main(String[] args){
+        System.out.println("这是非公共类里的main方法");
+    }
+}
